0eea8bbf33183d1b2d48c2ce5908ceff2825a9c0
user <user@WIN-1GI95NM0NJG>
2023-05-09 22:38:10 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(26, 16);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(100, 199);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.ControlAdded += new System.Windows.Forms.ControlEventHandler(this.listView1_ControlAdded);
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Учасники Гри";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 100;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Рахувати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(36, 250);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 23);
+            this.add.TabIndex = 2;
+            this.add.Text = "Добавити";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(381, 341);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel3.Controls.Add(this.comboBox1);
+            this.panel3.Location = new System.Drawing.Point(204, 18);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(155, 308);
+            this.panel3.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DisplayMember = "1";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Кіт на даху!",
+            "Скаче кінь великий!",
+            "ти навіть не барон!"});
+            this.comboBox1.Location = new System.Drawing.Point(22, 155);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "ти навіть не барон!";
+            this.comboBox1.ValueMember = " ";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.listView1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.add);
+            this.panel2.Location = new System.Drawing.Point(0, 18);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(155, 308);
+            this.panel2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(36, 279);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Видалити";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(359, 328);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Щиталочка";
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        internal System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+
+        string[] strName = { "Саша", "Аня", "Макс", "Пабло", "Дон", "Андре", "Аліна", "Росомаха" };
+        int randName;
+        Random random = new Random();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            randName = random.Next(strName.Length);
+            listView1.Items.Add(strName[randName]);
+            int j = listView1.Items.Count;
+            label1.Text = Convert.ToString(j);
+        }
+
+        private void listView1_ControlAdded(object sender, ControlEventArgs e)
+        {
+            
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count > 0)
+            {
+                listView1.Items.RemoveAt(listView1.Items.Count - 1);
+            }
+
+            int j = listView1.Items.Count;
+            label1.Text = Convert.ToString(j);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        string[] Accounting = { };
+        string[] Accounting1 = {"Цар","Імператор","Шах","Король","Великий князь","Фараон","А ти вийди геть","Ти навіть не \nбарон"};
+        string[] Accounting2 = { "Тихіше", "миші", "Кіт на даху", "Хто не чув", "Той і вийшов!" };
+        string[] Accounting3 = {"Хмари", "хмари!", "хмарии", "Хмари","Скаче кінь \n великий", "могутній","Через хмари \n скаче він","Хто не вірить – \n вийди геть!"};
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string selectedState = comboBox1.SelectedItem.ToString();
+            switch (selectedState)
+            {
+                case "ти навіть не барон!":
+                    Accounting = Accounting1;
+                    break;
+                case "Кіт на даху!":
+                    Accounting = Accounting2;
+                    break;
+                case "Скаче кінь великий!":
+                    Accounting = Accounting3;
+                    break;
+                default:
+                    return;
+            }
+            if (listView1.Items.Count == 0 || listView1.Items.Count == 1)
+            {
+                return;
+            }
+            
+            for (int i = 0; i < Accounting.Length; i++)
+            {
+                    int index = i % listView1.Items.Count;
+                    if (index < listView1.Items.Count)
+                    {
+                        listView1.Items[index].ForeColor = Color.Red;
+                    }
+
+                    if (Accounting[i] == Accounting.Last())
+                    {
+                        if (index < listView1.Items.Count)
+                        {
+                            listView1.Items.RemoveAt(index);
+                            int k = listView1.Items.Count;
+                            label1.Text = Convert.ToString(k);
+                        }
+                    }
+                
+
+                var t = Task.Delay(700);
+                Label label = new Label();
+                panel3.Controls.Add(label);
+                label.ForeColor = Color.Red;
+                label.AutoSize = true;
+                label.Font = new Font("Time New Romance", 10);
+                label.Location = new Point(36, 16);
+                label.Text = Accounting[i];
+                t.Wait();
+                if (index < listView1.Items.Count)
+                {
+                    listView1.Items[index].ForeColor = Color.Black;
+                }
+                label.Dispose();
+            }
+        }
+
+
+    }
+}
